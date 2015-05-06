4fd4434a6e36a25ff18f929dbb7b7a5957a99318
Martin Schwarz <martin.schwarz10@gmail.com>
2015-05-06 22:23:23 +0200

    minor adjustment

--- main.cpp
+++ main.cpp
@@ -8,12 +8,15 @@ int main(){
 	Clock observedClock;
 	ClockViewer viewer01("01");
 	ClockViewer viewer02("02");
+	ClockViewer viewer03("03");
 
 	observedClock.addObserver(&viewer01);
 	observedClock.addObserver(&viewer02);
+	observedClock.addObserver(&viewer03);
 
 	while(1){
 		observedClock.setTime();
+		std::cout << std::endl;
 		sleep(1);
 	}
 
